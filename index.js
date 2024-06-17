e02c391b06715f58e89c475249aed838fc340eaa
savannahmarshall <savvymarshall@gmail.com>
2024-06-17 11:48:51 -0700

    added functions for generating readme, writing readme file and initializing app

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const fs = require('fs');
 const inquirer = require ('inquirer');
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -18,7 +18,7 @@ const questions = [
         type: 'input',
         name: 'installation',
         message: 'Enter installation instructions here:',
-    }
+    },
     {
         type: 'input',
         name: 'usage',
@@ -52,11 +52,72 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const licenseBadges = {
+    MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    GPLv3: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    'Apache 2.0': '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    'BSD 3-Clause': '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+    None: '',
+};
 
-// TODO: Create a function to initialize app
-function init() {}
+// function to generate readme answers 
+function generateReadme(answers) {
+    const readmeContent = `# ${answers.projectTitle}
 
-// Function call to initialize app
+${licenseBadges[answers.license]}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at ${answers.email}. You can find more of my work at [github.com/${answers.githubUsername}](https://github.com/${answers.githubUsername}).
+`;
+
+return readmeContent;
+}
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+        } else {
+            console.log(`${fileName} generated successfully!`);
+        }
+    });
+}
+
+//function to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = generateReadme(answers);
+        writeToFile('README.md', readmeContent);
+    });
+}
+
+// function call to initialize app
 init();
+
